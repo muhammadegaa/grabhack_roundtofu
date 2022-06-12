007ecc39dd01edbf717a3b5d1a3b9d4e81357090
muhammad ega <simatupang.ega@gmail.com>
2022-06-12 21:38:41 +0700

    initial commit

---
+++ app/src/main/java/com/roundtofu/traditional_market/models/User.kt
@@ -0,0 +1,13 @@
+package com.roundtofu.traditional_market.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+        val uid: String,
+        val name: String,
+        val profileImageUrl: String?
+) : Parcelable {
+    constructor() : this("", "", "")
+}
